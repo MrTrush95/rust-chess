7cdad18c8e5e1555449f400f82ee32ebeb80f376
Martin Truchon <mtruchon@spiria.com>
2023-05-08 10:45:50 -0400

    Add board reference to move_gen and fix pawn attacks

--- src/move_generator.rs
+++ src/move_generator.rs
@@ -7,23 +7,27 @@ use crate::{
     rank::Rank,
 };
 
-pub struct MoveGenerator {
+pub struct MoveGenerator<'a> {
+    board: &'a Board,
     pub moves: Vec<ChessMove>,
 }
 
-impl MoveGenerator {
+impl MoveGenerator<'_> {
     pub fn new(board: &Board) -> MoveGenerator {
-        let mut move_generator = MoveGenerator { moves: vec![] };
+        let mut move_generator = MoveGenerator {
+            board,
+            moves: vec![],
+        };
 
-        move_generator.generate_legal_moves(board);
+        move_generator.generate_legal_moves();
 
         move_generator
     }
 
-    fn generate_legal_moves(&mut self, board: &Board) {
-        for (square_index, square) in board.squares.iter().enumerate() {
+    fn generate_legal_moves(&mut self) {
+        for (square_index, square) in self.board.squares.iter().enumerate() {
             let chess_moves = match square {
-                Some(piece) => self.generate_pieces_moves(board, piece, square_index as u8),
+                Some(piece) => self.generate_pieces_moves(piece, square_index as u8),
                 None => None,
             };
 
@@ -33,34 +37,24 @@ impl MoveGenerator {
         }
     }
 
-    fn generate_pieces_moves(
-        &self,
-        board: &Board,
-        piece: &Piece,
-        square_index: u8,
-    ) -> Option<Vec<ChessMove>> {
-        if piece.get_color() != board.side_to_move {
+    fn generate_pieces_moves(&self, piece: &Piece, square_index: u8) -> Option<Vec<ChessMove>> {
+        if piece.get_color() != self.board.side_to_move {
             return None;
         }
 
         let moves = match piece.get_type() {
-            PieceType::Pawn => self.generate_pawn_moves(board, piece, square_index),
-            PieceType::Knight => self.generate_knight_moves(board, piece, square_index),
-            PieceType::Bishop => self.generate_bishop_moves(board, piece, square_index),
-            PieceType::Rook => self.generate_rook_moves(board, piece, square_index),
-            PieceType::Queen => self.generate_queen_moves(board, piece, square_index),
-            PieceType::King => self.generate_king_moves(board, piece, square_index),
+            PieceType::Pawn => self.generate_pawn_moves(piece, square_index),
+            PieceType::Knight => self.generate_knight_moves(piece, square_index),
+            PieceType::Bishop => self.generate_bishop_moves(piece, square_index),
+            PieceType::Rook => self.generate_rook_moves(piece, square_index),
+            PieceType::Queen => self.generate_queen_moves(piece, square_index),
+            PieceType::King => self.generate_king_moves(piece, square_index),
         };
 
         Some(moves)
     }
 
-    fn generate_pawn_moves(
-        &self,
-        board: &Board,
-        piece: &Piece,
-        square_index: u8,
-    ) -> Vec<ChessMove> {
+    fn generate_pawn_moves(&self, piece: &Piece, square_index: u8) -> Vec<ChessMove> {
         let mut moves: Vec<ChessMove> = vec![];
 
         let forward_move: i32 = 8;
@@ -71,10 +65,10 @@ impl MoveGenerator {
         let offset: i32 = if piece.is_color(Color::White) { -1 } else { 1 };
 
         let mut quiet_moves: Vec<i32> = vec![forward_move * offset];
-        let mut attack_moves: Vec<i32> = vec![7, 9];
+        let mut attack_moves: Vec<i32> = vec![];
 
         let space_ahead_occupied =
-            board.squares[(square_index as i32 + (forward_move * offset)) as usize].is_some();
+            self.board.squares[(square_index as i32 + (forward_move * offset)) as usize].is_some();
 
         if rank.is_pawn_rank(piece.get_color()) && !space_ahead_occupied {
             quiet_moves.push(forward_move * 2 * offset);
@@ -90,7 +84,7 @@ impl MoveGenerator {
 
         for quiet_move in quiet_moves.iter() {
             let space_occupied =
-                board.squares[(square_index as i32 + *quiet_move) as usize].is_some();
+                self.board.squares[(square_index as i32 + *quiet_move) as usize].is_some();
 
             if !space_occupied {
                 moves.push(ChessMove::new(
@@ -102,7 +96,7 @@ impl MoveGenerator {
 
         for attack_move in attack_moves.iter() {
             let space_occupied =
-                board.squares[(square_index as i32 + *attack_move) as usize].is_some();
+                self.board.squares[(square_index as i32 + *attack_move) as usize].is_some();
 
             if space_occupied {
                 moves.push(ChessMove::new(
@@ -119,56 +113,31 @@ impl MoveGenerator {
         moves
     }
 
-    fn generate_knight_moves(
-        &self,
-        board: &Board,
-        piece: &Piece,
-        square_index: u8,
-    ) -> Vec<ChessMove> {
+    fn generate_knight_moves(&self, piece: &Piece, square_index: u8) -> Vec<ChessMove> {
         let moves: Vec<ChessMove> = vec![];
 
         moves
     }
 
-    fn generate_bishop_moves(
-        &self,
-        board: &Board,
-        piece: &Piece,
-        square_index: u8,
-    ) -> Vec<ChessMove> {
+    fn generate_bishop_moves(&self, piece: &Piece, square_index: u8) -> Vec<ChessMove> {
         let moves: Vec<ChessMove> = vec![];
 
         moves
     }
 
-    fn generate_rook_moves(
-        &self,
-        board: &Board,
-        piece: &Piece,
-        square_index: u8,
-    ) -> Vec<ChessMove> {
+    fn generate_rook_moves(&self, piece: &Piece, square_index: u8) -> Vec<ChessMove> {
         let moves: Vec<ChessMove> = vec![];
 
         moves
     }
 
-    fn generate_queen_moves(
-        &self,
-        board: &Board,
-        piece: &Piece,
-        square_index: u8,
-    ) -> Vec<ChessMove> {
+    fn generate_queen_moves(&self, piece: &Piece, square_index: u8) -> Vec<ChessMove> {
         let moves: Vec<ChessMove> = vec![];
 
         moves
     }
 
-    fn generate_king_moves(
-        &self,
-        board: &Board,
-        piece: &Piece,
-        square_index: u8,
-    ) -> Vec<ChessMove> {
+    fn generate_king_moves(&self, piece: &Piece, square_index: u8) -> Vec<ChessMove> {
         let moves: Vec<ChessMove> = vec![];
 
         moves
